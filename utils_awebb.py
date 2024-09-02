4c0d0414d6d16183faec861fed7d72c86a1acc28
Adrianna Webb <anna.m.webb110@gmail.com>
2024-09-01 20:39:51 -0500

    Iteration 3 - implementing global variables

--- utils_awebb.py
+++ utils_awebb.py
@@ -1,36 +1,46 @@
-# Online Python - IDE, Editor, Compiler, Interpreter
-
-''' ITERATION 1
+''' ITERATION 3
 
 Module: Webb Analytics - Reusable Module for My Data Analytics Projects
 
-This module provides a simple, reusable foundation for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
+This module provides a simple, reusable foundation for my analytics projects. .
 A good byline could be used in every Python analytics project we do.
 
 Process:
-
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_case.py might look like:
-
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
-   
-2. I'll declare a global variable for my byline string and just set it to some simple text.
-
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
-
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
-
 I'll test it in an online interpreter to ensure this version runs correctly before continuing.
+Process:
+In this third iteration, I declare additional variables to show skills with different data types
 '''
+#######################################
+# Declare global variables - keep byline at the end
+# We will use this information in a smarter byline
+#######################################
 
-#####################################
+# Boolean variable to indicate if the company has international clients
+has_international_clients: bool = True
+
+# Integer variable for the number of years in operation
+years_in_operation: int = 10
+
+# List of strings representing the skills offered by the company
+skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
+
+# List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
+
+#######################################
 # Declare a global variable named byline.
-#####################################
+# Make it a multiline f-string to show our information.
+#######################################
 
-byline: str = 'Webb Analytics: Analytics for the Web, by Webb'
+byline: str = f"""
+-------------------------------------------------
+Webb Analytics: Analytics for the Web, by Webb
+-------------------------------------------------
+Has International Clients: {has_international_clients}
+Years in Operation:        {years_in_operation}
+Skills Offered:            {skills_offered}
+Client Satisfaction Scores: {client_satisfaction_scores}
+"""
 
 #####################################
 # Define the get_byline() Function
